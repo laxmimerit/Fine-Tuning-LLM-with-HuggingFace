0ceff0a20b0018958efc4eeac1aa281af828a597
laxmimerit <laxmimerit@gmail.com>
2024-10-02 15:37:23 +0530

    complete streamlit application

--- 05-Fine_Tuning_BERT_for_Sentiment/app.py
+++ 05-Fine_Tuning_BERT_for_Sentiment/app.py
@@ -8,9 +8,10 @@ from transformers import pipeline
 
 st.title("Fine Tuning BERT for Twitter Tweets for Multi Class Sentiment Classification")
 
+
 classifier = pipeline('text-classification', model= 'bert-base-uncased-sentiment-model')
 
-text = st.text_area("Enter some text")
+text = st.text_area("Enter Your Tweet Here")
 
 if st.button("Predict"):
     result = classifier(text)
